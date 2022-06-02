52d0941fef0456f2781fe6dea12efcc4c9d47017
Angelina <angelinarwhite@gmail.com>
2022-06-01 20:56:37 -0400

    make header move in from right

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,17 @@
+import { useEffect } from "react"
+import { gsap } from "gsap";
+
 function Home()
 {
+    //make title come in from right
+    useEffect(() =>
+    {
+        gsap.from("#homeTitle", {
+            x: 50,
+            duration: 1
+        })
+    }, [])
+
     return (
         <div>
             <h1 id="homeTitle">Home</h1>
